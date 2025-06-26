07e9f5e70f91506197bb613ba78beba37c4d6243
galibmudasir <mudasirgalib5@gmail.com>
2025-06-26 19:43:14 +0700

    check integrasi supabase

--- src/libs/supabase.js
+++ src/libs/supabase.js
@@ -4,3 +4,6 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+console.log("🔧 Supabase URL:", supabaseUrl);
+console.log("🔧 Supabase Key:", supabaseAnonKey ? "[TERSEDIA]" : "[TIDAK ADA]");
