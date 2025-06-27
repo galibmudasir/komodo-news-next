864ac59166a57768495b6f847d693fc4a17a828a
galibmudasir <mudasirgalib5@gmail.com>
2025-06-27 16:08:17 +0700

    penambahan sitemap dan tag sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://komodo-news.vercel.app/", // Ganti dengan domain kamu
+  generateRobotsTxt: true, // true = auto bikin robots.txt
+  // Optional: exclude halaman tertentu
+  // exclude: ['/admin', '/login'],
+};
